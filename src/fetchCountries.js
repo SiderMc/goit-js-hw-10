a363380ca1405533eb8c3e072c589c12157e3c15
Stepan <lkrossl14@gmail.com>
2023-05-24 13:04:38 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,11 @@
-import { getCountry } from './index.js';
-import { getValues } from './index.js';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-export { getData };
-
-function getData() {
-  return fetch(`https://restcountries.com/v3.1/name/${getCountry()}`)
-    .then(response => {
-      if (response.status === 404) {
-        throw new Error('Not found');
+function getData(country) {
+  return fetch(`https://restcountries.com/v3/name/${country}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
       }
       return response.json();
-    })
-    .then(data => getValues(data))
-    .catch(error => {
-      console.error(error);
-      Notify.failure('Oops, there is no country with that name');
-    });
+    }
+  );
 }
+export { getData };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 Notify.init({
   position: 'center-top',
   fontSize: '18px',
-  timeout: 3000,
+  timeout: 2000,
   cssAnimationStyle: 'from-top',
   width: '350px',
 });
@@ -19,18 +19,31 @@ const info = document.querySelector('.country-info');
 form.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput() {
-  const country = getCountry();
-  if (country) {
-    getData(country);
-  } else {
-    clearLists();
+  const country = form.value.trim();
+  getData(country).then(handleSuccess).catch(handleError);
+}
+
+function handleSuccess(data) {
+  clearLists();
+
+  if (data.length === 1) {
+    info.innerHTML = getSingleCountryInfo(data[0]);
+    info.classList.add('visible');
+  } else if (data.length > 1) {
+    if (data.length <= 10 && data.length >= 2) {
+      list.innerHTML = data.map(getMultipleCountryInfo).join('');
+    } else {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    }
   }
 }
 
-function getCountry() {
-  if (form.value.trim() !== '') {
-    return form.value;
+function handleError(error) {
+  clearLists();
+  if (error.message === '404') {
+    return Notify.failure('Oops, there is no country with that name');
   }
+  Notify.failure(`${error.message}`);
 }
 
 function clearLists() {
@@ -39,29 +52,11 @@ function clearLists() {
   info.classList.remove('visible');
 }
 
-function getValues(data) {
-  clearLists();
-
-  if (data.length === 1) {
-    info.innerHTML = getSingleCountryInfo(data[0]);
-    info.classList.add('visible');
-  } else if (data.length > 1) {
-    if (data.length <= 10) {
-      list.innerHTML = data.map(getMultipleCountryInfo).join('');
-      list.classList.add('animate');
-    } else {
-      Notify.info('Too many matches found. Please enter a more specific name.');
-    }
-  }
-}
-
-function getMultipleCountryInfo({
-  flags: { svg },
-  name: { official, common },
-}) {
+function getMultipleCountryInfo({ flags, name: { official, common } }) {
+  const svgBig = flags.find(flag => flag.endsWith('.svg'));
   return `
     <li class="country-list-item">
-      <img class="country-small-flag" src="${svg}" alt="${official}">
+      <img class="country-small-flag" src="${svgBig}" alt="${official}">
       <p class="country-small-text">${common}</p>
     </li>
   `;
@@ -71,25 +66,25 @@ function getSingleCountryInfo({
   capital,
   population,
   languages,
-  flags: { svg },
+  flags,
   name: { official },
 }) {
+  const svg = flags.find(flag => flag.endsWith('.svg'));
+
   return `
     <img class="country-big-flag" src="${svg}" alt="${official}">
     <h2 class="country-name">${official}</h2>
     <p class="country-title">
-      Capital :
+      Capital:
       <span class="country-item">${capital}</span>
     </p>
     <p class="country-title">
-      Population :
+      Population:
       <span class="country-item">${population}</span>
     </p>
     <p class="country-title">
-      Languages :
-      <span class="country-item">${Object.values(languages)}</span>
+      Languages:
+      <span class="country-item">${Object.values(languages).join(',')}</span>
     </p>
   `;
 }
-
-export { getCountry, getValues };
